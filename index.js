a20a1cca6e1478d2bc9c1b42c87dd3314ff379dc
Syaf <B022210128@student.utem.edu.my>
2024-04-04 14:48:05 +0800

    Refactor code and update dependencies

--- index.js
+++ index.js
@@ -29,9 +29,8 @@ app.delete('/profile', (req, res) => {
     res.send('Profile deleted successfully');
 })
 
-app.get('/', (req, res) => {
-   res.send('Hello World!')
-})
+//
+
 
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`)
